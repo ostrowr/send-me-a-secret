60bfce30d9de92f2f20f48d0914c14645f2c8ff0
Robbie Ostrow <ostrowr@gmail.com>
2021-03-21 12:29:23 -0700

    add wasm

---
+++ cmd/wasm/registerEncryptor.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"syscall/js"
+
+	"github.com/ostrowr/send-me-a-secret/internal/encryptor"
+)
+
+func encryptWrapper() js.Func {
+	f := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "No message provided."
+		}
+		message := args[0].String()
+		encrypted, err := encryptor.Encrypt([]byte(message))
+		if err != nil {
+			return err.Error()
+		}
+		return encrypted
+	})
+	return f
+}
+
+func main() {
+	fmt.Println("Go Web Assembly")
+	js.Global().Set("encrypt", encryptWrapper())
+	<-make(chan bool) // never exit
+}
